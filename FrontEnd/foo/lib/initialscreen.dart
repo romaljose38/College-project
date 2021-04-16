1208e00615f41be7881e6945c03a9faf04c34161
theunixdisaster <pranavsdreams@gmail.com>
2021-04-16 12:17:18 +0530

    Reduced initial evoke time gap

--- FrontEnd/foo/lib/initialscreen.dart
+++ FrontEnd/foo/lib/initialscreen.dart
@@ -9,45 +9,45 @@ class Renderer extends StatefulWidget {
 }
 
 class _RendererState extends State<Renderer> {
-
   Timer timer;
-  bool isConnected=false;
+  bool isConnected = true;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    timer = Timer.periodic(Duration(seconds: 5), (Timer t) => _checkConnectionStatus());
+    timer = Timer.periodic(
+        Duration(seconds: 2), (Timer t) => _checkConnectionStatus());
   }
 
-  void _checkConnectionStatus() async{
- bool result = await DataConnectionChecker().hasConnection;  
+  void _checkConnectionStatus() async {
+    bool result = await DataConnectionChecker().hasConnection;
 
-      if(result == true) {
-        setState((){
-          isConnected=true;
-        });
-        print('YAY! Free cute dog pics!');
-      } else {
-         setState((){
-          isConnected=false;
-        });
-      }
-  } 
+    if (result == true) {
+      setState(() {
+        isConnected = true;
+      });
+      print('YAY! Free cute dog pics!');
+    } else {
+      setState(() {
+        isConnected = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-   
-   return isConnected?RegisterView():NoConnectionView();
+    return isConnected ? RegisterView() : NoConnectionView();
+  }
 }
-}
-
 
 class NoConnectionView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-        body:Center(child: Text("No network access",style:TextStyle(color:Colors.black,fontSize: 25)),)
-    );
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Text("No network access",
+              style: TextStyle(color: Colors.black, fontSize: 25)),
+        ));
   }
 }
