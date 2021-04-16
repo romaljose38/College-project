655b8f49307893f25d5319a3e9f8db32347157be
romaljose38 <romaljose38@gmail.com>
2021-04-16 11:42:48 +0530

    Main.dart update

--- FrontEnd/foo/lib/main.dart
+++ FrontEnd/foo/lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 //import 'package:foo/Login/login.dart';
 import 'Register/router.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:data_connection_checker/data_connection_checker.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,7 +20,47 @@ class MyApp extends StatelessWidget {
       title: 'Foo Register',
       theme: ThemeData.dark(),
       onGenerateRoute: generateRoute,
-      // home:LoginScreen(),
+      home:Renderer(),
+    );
+  }
+}
+
+
+class Renderer extends StatefulWidget {
+  @override
+  _RendererState createState() => _RendererState();
+}
+
+class _RendererState extends State<Renderer> {
+
+  Timer timer;
+  bool isConnected;
+
+  @override
+  void initState(){
+    super.initState();
+    timer = Timer.periodic(Duration(seconds: 5), (Timer t) => _checkConnectionStatus());
+  }
+
+  void _checkConnectionStatus() async{
+ bool result = await DataConnectionChecker().hasConnection;  
+
+      if(result == true) {
+        setState((){
+          isConnected=true;
+        });
+        print('YAY! Free cute dog pics!');
+      } else {
+         setState((){
+          isConnected=false;
+        });
+      }
+  } 
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
     );
   }
 }
